2e9d2c48d89f012edb486d66156d54cdad82b976
amidman2004 <dimavoronkov04@gmail.com>
2021-12-03 16:27:28 +0400

    Voronkov03.12.2021

---
+++ app/src/main/java/com/example/testvoronkov/ui/theme/Theme.kt
@@ -0,0 +1,47 @@
+package com.example.testvoronkov.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = gol0,
+    primaryVariant = gol0,
+    secondary = gol0
+)
+
+private val LightColorPalette = lightColors(
+    primary = gol0,
+    primaryVariant = gol0,
+    secondary = gol0
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun TestVoronkovTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
