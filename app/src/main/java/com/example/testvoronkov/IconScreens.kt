442bcd5491f1a238391965fc3459688900ec6440
uservsuservs <uservsuservs@yandex.ru>
2021-12-04 15:39:54 +0400

    BEL

--- app/src/main/java/com/example/testvoronkov/IconScreens.kt
+++ app/src/main/java/com/example/testvoronkov/IconScreens.kt
@@ -10,16 +10,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.core.content.ContextCompat
+import com.example.testvoronkov.ui.theme.biruzoviy
 import com.example.testvoronkov.ui.theme.gol1
 
 
 sealed class IconScreens(val id: String, val label:String, val icon: Int, var color: Color) {
 
     //Bottom Nav
-    object Home : IconScreens("home", "Home",R.drawable.ic_baseline_home_24, gol1)
+    object Home : IconScreens("home", "Home",R.drawable.ic_baseline_home_24, biruzoviy)
     object Test : IconScreens("test", "Test",R.drawable.ic_baseline_notes_24,Color.Gray)
-    object CheckList : IconScreens("check", "CheckTest", R.drawable.ic_baseline_checklist_rtl_24,
-        Color.Gray)
     object Account : IconScreens("profile", "Profile", R.drawable.ic_baseline_account_box_24, Color.Gray)
 
 
@@ -27,6 +26,5 @@ sealed class IconScreens(val id: String, val label:String, val icon: Int, var co
 val list = listOf(
     IconScreens.Home,
     IconScreens.Test,
-    IconScreens.CheckList,
     IconScreens.Account
 )
--- app/src/main/java/com/example/testvoronkov/MainActivity.kt
+++ app/src/main/java/com/example/testvoronkov/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.testvoronkov
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
@@ -14,14 +16,17 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.pointerInput
@@ -29,11 +34,24 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.FileProvider
 import androidx.navigation.NavHostController
 import com.example.testvoronkov.ui.theme.*
+import java.io.File
+import java.util.*
 
 data class icons(var id:String,var title:String,var paint : Int)
 class MainActivity : ComponentActivity() {
+    val urik = mutableStateOf<Uri?>(null)
+    val select = registerForActivityResult(ActivityResultContracts.GetContent()){ uri ->
+        urik.value = uri
+    }
+    var uri: Uri? = null
+    val take = registerForActivityResult(ActivityResultContracts.TakePicture()){
+        if (it){
+            urik.value = uri
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -47,54 +65,104 @@ class MainActivity : ComponentActivity() {
                 style = currentStyle.value,
                 darkThem = isDarkMode.value
             ) {
-                BottomBar(navController = NavHostController(applicationContext))
+                BottomBar(navController = NavHostController(applicationContext), list,"FIO")
             }
         }
     }
-}
+
 
 @Composable
-fun BottomBar(navController: NavHostController,items :List<IconScreens> = list) {
+fun BottomBar(navController: NavHostController,items :List<IconScreens> = list,FIO: String) {
     val current = remember {
         mutableStateOf("home")
     }
+    val context = LocalContext.current
 
-    Scaffold(bottomBar = {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = FIO)}, backgroundColor = biruzoviy, navigationIcon = {
+                Box(modifier = Modifier.background(shape = CircleShape,
+                    color = Color.Black).size(40.dp)){
+                    IconButton(onClick = {current.value = "profile"
+                        when(current.value){
+                            "home" ->{
+                                IconScreens.Home.color =  biruzoviy
+                                IconScreens.Test.color =  Color.Gray
+                                IconScreens.Account.color =  Color.Gray
+
+                            }
+                            "test" -> {
+                                IconScreens.Home.color =  Color.Gray
+                                IconScreens.Test.color =  biruzoviy
+                                IconScreens.Account.color =  Color.Gray
+                            }
+                            "profile" -> {
+                                IconScreens.Home.color =  Color.Gray
+                                IconScreens.Test.color =  Color.Gray
+                                IconScreens.Account.color =  biruzoviy
+                            }
+                        }},Modifier.background(shape = CircleShape,
+                        color = Color.Black).size(40.dp)) {
+                        Image(painter = painterResource(id = R.drawable.image), contentDescription = null,Modifier.background(shape = CircleShape,
+                            color = Color.Black
+                        ).clip(CircleShape), )
+                    }}
+
+            })
 
 
-        BottomNavigation(backgroundColor = JetHabbitTheme.colors.primaryBackground, contentColor = Color.White){
+        },
+        bottomBar = {
 
-            items.forEach{ item->
 
-                BottomNavigationItem(
-                    label = {
-                        Text(text = item.label, color = item.color)
-                    },
-                    selected = item.id == current.value,
-                    icon = {
-                        Icon(painter = painterResource(id = item.icon), contentDescription = item.label, tint = item.color)
-                    },
-                    onClick = {
-                        current.value = item.id
-                        IconScreens.Home.color =  Color.Gray
-                        IconScreens.Test.color =  Color.Gray
-                        IconScreens.CheckList.color =  Color.Gray
-                        IconScreens.Account.color =  Color.Gray
-                        item.color = gol1
+            BottomNavigation(backgroundColor = gol0, contentColor = Color.White){
 
-                    }
-                ) 
+                items.forEach{ item->
+
+                    BottomNavigationItem(
+                        label = {
+                            Text(text = item.label, color = item.color)
+                        },
+                        selected = item.id == current.value,
+                        icon = {
+                            Icon(painter = painterResource(id = item.icon), contentDescription = item.label, tint = item.color)
+                        },
+                        onClick = {
+                            current.value = item.id
+                            when(current.value){
+                                "home" ->{
+                                    IconScreens.Home.color =  biruzoviy
+                                    IconScreens.Test.color =  Color.Gray
+                                    IconScreens.Account.color =  Color.Gray
+
+                                }
+                                "test" -> {
+                                    IconScreens.Home.color =  Color.Gray
+                                    IconScreens.Test.color =  biruzoviy
+                                    IconScreens.Account.color =  Color.Gray
+                                }
+                                "profile" -> {
+                                    IconScreens.Home.color =  Color.Gray
+                                    IconScreens.Test.color =  Color.Gray
+                                    IconScreens.Account.color =  biruzoviy
+                                }
+                            }
+
+
+
+                        }
+                    )
+                }
             }
-        }
-    }) {
+        }) {
         when(current.value){
             "home" -> Home()
             "test" -> Test()
-            "check" -> TestCheck()
-            "profile" -> Account()
+            "profile" -> Account("FIO")
         }
     }
 }
+
 
 
 @Composable
@@ -236,12 +304,90 @@ fun LazyTest(id:String,name:String,Description:String,Nominations:String,partici
         }
     }
 }
+
+
 @Composable
-fun TestCheck() {
-    Text(text = "TESTCHECK")
-}
-@Composable
-fun Account() {
-    Text(text = "ACCOUNT")
+fun Account(FIO:String) {
+    val dialog = remember {
+        mutableStateOf(false)
+    }
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White)){
+        Column(modifier = Modifier.fillMaxSize()) {
+            Spacer(modifier = Modifier.size(30.dp))
+            Image(painter = painterResource(id = R.drawable.image), contentDescription = null,
+                Modifier
+                    .background(
+                        Color.White, shape = CircleShape
+                    )
+                    .align(Alignment.CenterHorizontally)
+                    .size(250.dp))
+            Spacer(modifier = Modifier.size(20.dp))
+            Text(text = FIO,modifier = Modifier.align(Alignment.CenterHorizontally))
+
+        }
+        Row(modifier = Modifier
+            .align(Alignment.BottomCenter)
+            .padding(bottom = 100.dp)) {
+
+            Button(onClick = {dialog.value = true}, modifier = Modifier
+                .size(200.dp, 60.dp)
+                , colors = ButtonDefaults.buttonColors(
+                    backgroundColor = biruzoviy
+                )) {
+                Text(text = "ChangePhoto", color = Color.White)
+            }
+            if (dialog.value){
+                dialog(dialog = dialog)
+            }
+        }
+    }
 }
 
+@Composable
+fun dialog(dialog: MutableState<Boolean>) {
+    if(dialog.value){
+        AlertDialog(onDismissRequest = {dialog.value = false}, buttons = {
+            Row(Modifier.height(80.dp)) {
+                Spacer(modifier = Modifier.size(5.dp))
+                Button(onClick = {select.launch("image/*")},
+                    modifier = Modifier
+                        .size(160.dp, 70.dp)
+                        .align(Alignment.CenterVertically),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = biruzoviy
+                    )
+                ) {
+                    Text(text = "Change From Gallery", color = Color.White)
+                }
+                Spacer(modifier = Modifier.size(20.dp))
+                Button(onClick = {
+                    val file = File(
+                        filesDir,
+                        UUID
+                            .randomUUID()
+                            .toString() + ".jpg"
+                    )
+                    uri = FileProvider.getUriForFile(
+                        applicationContext,
+                        "com.example.testvoronkov.fileprovider",
+                        file
+                    )
+                    take.launch(uri)},
+                    modifier = Modifier
+                        .size(160.dp, 70.dp)
+                        .align(Alignment.CenterVertically)
+                    ,colors = ButtonDefaults.buttonColors(
+                        backgroundColor = biruzoviy
+                    )) {
+                    Text(text = "Change From Camera", color = Color.White)
+                }
+                Spacer(modifier = Modifier.size(5.dp))
+            }
+        })
+    }
+
+}
+
+}
